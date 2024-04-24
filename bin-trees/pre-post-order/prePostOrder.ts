1a9a582a9e3cc41baaf264f828ca0923010fb403
Jazz Cheema <jaskamalc@gmail.com>
2024-04-24 15:08:27 -0700

    finished first exercise of binary tree

--- bin-trees/pre-post-order/prePostOrder.ts
+++ bin-trees/pre-post-order/prePostOrder.ts
@@ -4,15 +4,34 @@ import { BNodeNum } from "../common/bintree";
  * Returns an array of values of visited nodes. */
 
 function preOrder(node: BNodeNum | null): number[] {
-  return [42];
-}
+  if (!node) return [];
 
+  const vals = [];
+
+  //push current val -> then push left -> push right
+  vals.push(node.val);
+
+  vals.push(...preOrder(node.lnode));
+  vals.push(...preOrder(node.rnode));
+
+  return vals;
+}
 
 /** dfsPostOrder(): Traverse from the invoking node using post-order DFS.
  * Returns an array of values of visited nodes. */
 
 function postOrder(node: BNodeNum | null): number[] {
-  return [42];
+  if (!node) return [];
+
+  const vals = [];
+
+  //push current val -> then push left -> push right
+
+  vals.push(...postOrder(node.lnode));
+  vals.push(...postOrder(node.rnode));
+
+  vals.push(node.val);
+  return vals;
 }
 
 export { preOrder, postOrder };
