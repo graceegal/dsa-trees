bc3a4f10159758a81ca05c2488fff00fce6d8043
Gracee Gallivan <gracee.gallivan@gmail.com>
2024-04-24 14:55:59 -0700

    completed pre and post order

--- trees/pre-post-order/prePostOrder.ts
+++ trees/pre-post-order/prePostOrder.ts
@@ -4,15 +4,49 @@ import { TreeNodeNum } from "../common/tree";
  * Returns an array of values of visited nodes. */
 
 function preOrder(node: TreeNodeNum | null): number[] {
-  return [42];
+  if (!node) return [];
+
+  const preOrdered = [];
+
+  preOrdered.push(node.val);
+
+  for (const child of node.children) {
+    preOrdered.push(...preOrder(child));
+  }
+
+  return preOrdered;
 }
+
+//    1       [ 1,  ]
+//    |
+//    2       [2]
+
+//            1       [1, 3, 7, 9, 5, 4, 6]
+//          / | \
+//         3  7  4
+//            |   \
+//            9   6
+//            |
+//            5
+
+
 
 
 /** dfsPostOrder(): Traverse from the invoking node using post-order DFS.
  * Returns an array of values of visited nodes. */
 
 function postOrder(node: TreeNodeNum | null): number[] {
-  return [42];
+  if (!node) return [];
+
+  const postOrdered = [];
+
+  for (const child of node.children) {
+    postOrdered.push(...postOrder(child));
+  }
+
+  postOrdered.push(node.val);
+
+  return postOrdered;
 }
 
 export { preOrder, postOrder };
